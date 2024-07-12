a6594de8286e22c60d32c36baca5db058789c8fc
PashchenkoIvan <ipaschenko09@gmail.com>
2024-07-12 18:07:57 +0300

    Created a CRUD class to manage Realm storage

--- PurchaceApp/RealmHelper/RealmHelper.swift
+++ PurchaceApp/RealmHelper/RealmHelper.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import RealmSwift
+
+class PurchaseObject: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var text: String
+    @Persisted var cost: Double
+    @Persisted var timestamp: Date
+}
+
+extension PurchaseObject {
+    static func getAllPurchases() -> [PurchaseObject] {
+        let realm = try! Realm()
+        return Array(realm.objects(PurchaseObject.self))
+    }
+}
+
+extension PurchaseObject {
+    static func addPurchase(text: String = "Nothing", cost: Double = 0.0) {
+        let newPurchase = PurchaseObject()
+        
+        newPurchase.text = text
+        newPurchase.cost = cost
+        newPurchase.timestamp = Date()
+        
+        let realm = try! Realm()
+        
+        try! realm.write {
+            realm.add(newPurchase)
+        }
+    }
+}
+
+extension PurchaseObject {
+    static func deletePurchase(withId id: ObjectId) {
+        let realm = try! Realm()
+        if let purchaseToDelete = realm.object(ofType: PurchaseObject.self, forPrimaryKey: id) {
+            try! realm.write {
+                realm.delete(purchaseToDelete)
+            }
+        }
+    }
+}
+
+
