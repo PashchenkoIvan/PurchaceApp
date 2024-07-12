1ce179ea4dea3f9d4df94e5bcf1e24adc9a07aa7
PashchenkoIvan <ipaschenko09@gmail.com>
2024-07-12 17:58:08 +0300

    Initial Commit

---
+++ PurchaceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PurchaceApp
+//
+//  Created by Пащенко Иван on 12.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
